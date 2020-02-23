105f7ea9495371f01fc4f98fe062e161d2f67116
MitchStan <31759531+MitchStan@users.noreply.github.com>
2020-02-22 22:11:33 -0800

    Start

---
+++ AI Stuff/Assets/TestTask.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+namespace BehaviorDesigner.Runtime.Tasks
+{
+	[TaskIcon("Assets/Png/{SkinColor}Nuclear.png")]
+	[TaskCategory("Test")]
+	public class TestTask : Action
+	{
+		public SharedString text;
+		public override TaskStatus OnUpdate()
+		{
+			Debug.Log(text);
+			return TaskStatus.Success;
+		}
+	}
+}
+
